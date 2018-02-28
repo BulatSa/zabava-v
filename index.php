f7b68330b284247d85189ce1dedf23d5feb65e88
Bulat <sadikovba@gmail.com>
2018-02-28 14:53:22 +0300

    Schools Final

--- index.php
+++ index.php
@@ -22,6 +22,7 @@
 					<li><a href="programms.php">Шоу-программы</a></li>
 					<li><a href="calendar.php">Календарные праздники</a></li>
 					<li><a href="additions.php">Добавки</a></li>
+					<li><a href="schools.php">Для школ и классов</a></li>
 				</ul>
 			</div>
 		</div>
---
+++ schools.php
@@ -0,0 +1,112 @@
+<?php include('header.php'); ?>
+
+
+<section class="animators-list-sec def-sec">
+	<div class="animators-list container">
+		<div class="page-title">
+			<p class="h2">Для школ и классов</p>
+			<p class="page-title__descr">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet.</p>
+		</div>
+		<div class="additions-list additions-list--center">
+			<div class="addition anim">
+				<a href="#">
+					<span class="addition__img">
+						<img src="img/additions/addition-1.jpg" alt="">
+						<span class="addition__descr">Приглашая фотографа на Ваш праздник, Вы получите более 150-200 обработанных фотографий, которые оставят неизгладимый след в вашей памяти о столь знаменательном событии.</span>
+					</span>
+					<span class="h5">Шоу-экскурсии с классом на выпускной</span>
+					<span class="addition__more">Подробнее</span>
+				</a>
+			</div>
+			<div class="addition anim">
+				<a href="#">
+					<span class="addition__img">
+						<img src="img/additions/addition-2.jpg" alt="">
+						<span class="addition__descr">Приглашая фотографа на Ваш праздник, Вы получите более 150-200 обработанных фотографий, которые оставят неизгладимый след в вашей памяти о столь знаменательном событии.</span>
+					</span>
+					<span class="h5">Новогодние шоу-экскурсии для классов</span>
+					<span class="addition__more">Подробнее</span>
+				</a>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<section class="additions-sec def-sec-top def-sec-waved-bottom wave-gray-top wave-gray-bottom">
+	<div class="additions container">
+		<div class="page-title page-title--mini">
+			<p class="h2">Прошлые программы</p>
+			<p class="page-title__descr">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet.</p>
+		</div>
+		<div class="additions__list">
+			<div class="additions__item addition anim">
+				<a href="#">
+				<span class="addition__img">
+					<img src="img/programms/programm-13.jpg" alt="">
+					<span class="addition__descr">Приглашая фотографа на Ваш праздник, Вы получите более 150-200 обработанных фотографий, которые оставят неизгладимый след в вашей памяти о столь знаменательном событии.</span>
+				</span>
+					<span class="h5">Новогодние шоу-экскурсии для классов</span>
+					<span class="addition__more">Подробнее</span>
+				</a>
+			</div>
+			<div class="additions__item addition anim">
+				<a href="#">
+				<span class="addition__img">
+					<img src="img/programms/programm-13.jpg" alt="">
+					<span class="addition__descr">Приглашая фотографа на Ваш праздник, Вы получите более 150-200 обработанных фотографий, которые оставят неизгладимый след в вашей памяти о столь знаменательном событии.</span>
+				</span>
+					<span class="h5">Новогодние шоу-экскурсии для классов</span>
+					<span class="addition__more">Подробнее</span>
+				</a>
+			</div>
+			<div class="additions__item addition anim">
+				<a href="#">
+				<span class="addition__img">
+					<img src="img/programms/programm-13.jpg" alt="">
+					<span class="addition__descr">Приглашая фотографа на Ваш праздник, Вы получите более 150-200 обработанных фотографий, которые оставят неизгладимый след в вашей памяти о столь знаменательном событии.</span>
+				</span>
+					<span class="h5">Новогодние шоу-экскурсии для классов</span>
+					<span class="addition__more">Подробнее</span>
+				</a>
+			</div>
+			<div class="additions__item addition anim">
+				<a href="#">
+				<span class="addition__img">
+					<img src="img/programms/programm-13.jpg" alt="">
+					<span class="addition__descr">Приглашая фотографа на Ваш праздник, Вы получите более 150-200 обработанных фотографий, которые оставят неизгладимый след в вашей памяти о столь знаменательном событии.</span>
+				</span>
+					<span class="h5">Новогодние шоу-экскурсии для классов</span>
+					<span class="addition__more">Подробнее</span>
+				</a>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<section class="form-bottom-sec def-sec" style="background-image: url('../img/form-bottom/bg-additions.jpg')">
+	<div class="form-bottom container">
+		<div class="page-title page-title--mini">
+			<p class="h2">Не знаете, что выбрать?<span class="h3">оставьте заявку, и&nbsp;наш менеджер свяжется с&nbsp;вами в&nbsp;ближайшее время </span></p>
+		</div>
+		<div class="form-bottom__input">
+			<form class="ajax-form">
+				<input type="text" class="input-text input-text--half" placeholder="имя*" name="name" data-req="true">
+				<input type="text" class="input-text input-text--half" placeholder="телефон*" name="phone" data-req="true">
+				<input type="text" class="input-text" placeholder="E-mail" name="phone">
+				<textarea class="input-textarea" name="comment" placeholder="Дополнения"></textarea>
+				<div class="form-submit">
+					<label class="style-checkbox">
+						<input type="checkbox" name="user_policy" data-label="Согласен с условиями" value="yes" data-req="true" checked="">
+						<span>Даю согласие на обработку персональных данных и соглашаюсь с условиями <a href="#" target="_blank">политики конфиденциальности</a>.</span>
+					</label>
+					<button type="submit" class="btn">Отправить</button>
+				</div>
+			</form>
+		</div>
+	</div>
+</section>
+
+
+<?php include('footer.php'); ?>
